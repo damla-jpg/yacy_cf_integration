7cfb05a912d29de52c1bc620f9aad8da3567a755
dul800 <d.ural@student.vu.nl>
2024-07-09 14:35:51 +0200

    minor changes in explore page

--- app/src/pages/Explore.js
+++ app/src/pages/Explore.js
@@ -1,10 +1,31 @@
 import React from 'react';
+const apiPort = process.env.REACT_APP_API_PORT;
 
 const Explore = () => {
+    // const [queryPredictions, setQueryPredictions] = React.useState([]);
+
+    React.useEffect(() => {
+        function getPredictions() {
+            // get predictions from the server
+            fetch(`http://localhost:${apiPort}/api/fetch_predictions`)
+                .then(response => response.json())
+                .then(data => {
+                    console.log(data);
+                    return data;
+                })
+                .catch(error => {
+                    console.error('Error:', error);
+                });
+        }
+
+        getPredictions();
+    }, []);
+
     return (
         <div>
             <h1> Explore </h1>
-            
+
+
         </div>
     );
 };
